57199c1fa83b8c569d8328c018c909426dad11c8
TheSandeepRaj <sandeeprajghimire@gmail.com>
2025-01-02 15:11:03 +0000

    Add backend routes and connect to MongoDB Finalized

--- backend/server.js
+++ backend/server.js
@@ -1,42 +1,26 @@
 import express from "express";
 import dotenv from "dotenv";
-import mongoose from "mongoose";
 import { connectDB } from "./config/db.js";
-import Product from "./models/product.js";
+import productRoutes from "./routes/product.routes.js";
 
 dotenv.config();
 const app = express();
 app.use(express.json());
 
-connectDB();
-
+app.use("api/products", productRoutes);
 const uri = process.env.MONGO_URI;
 
 app.get("/", (req, res) => {
   res.send("Welcome to the API");
 });
 
-// Route to add product
-app.post("/api/products", async (req, res) => {
-  const product = req.body;
+const startServer = async () => {
+  await connectDB(); // Ensure the database connection is established before starting the server
 
-  if (!product.name || !product.price || !product.image) {
-    return res
-      .status(400)
-      .json({ success: false, message: "All fields are required" });
-  }
+  const port = process.env.PORT || 3000;
+  app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
+};
 
-  try {
-    const newProduct = new Product(product);
-    await newProduct.save();
-    res.status(201).json({ success: true, data: newProduct });
-  } catch (error) {
-    console.error("Error saving product:", error.message);
-    res.status(500).json({ success: false, message: "Server error" });
-  }
-});
-
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+startServer();
